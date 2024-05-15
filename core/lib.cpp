3032855d1196ef64314d23e0d1d25f97a5748d23
k0tran <>
2024-05-15 21:42:26 +0300

    Added package substract (not tested)

--- core/lib.cpp
+++ core/lib.cpp
@@ -12,3 +12,38 @@ std::optional<json> ALTAPI::get(const std::string &path) const {
         return json::parse(r.text);
     return {};
 }
+
+BranchData::BranchData(json source) {
+    for (size_t i = 0; i < source["packages"].size(); ++i) {
+        ojson stripped{std::move(source["packages"][i])};
+        std::string arch = stripped["arch"];
+        std::string name = stripped["name"];
+        stripped.erase("arch");
+        stripped.erase("name");
+
+        data[arch][name] = std::move(stripped);
+    }
+}
+
+json BranchData::substract(const BranchData &other) const {
+    json result = json::array();
+
+    // Cycle over arch
+    for (const auto &it : data.items()) {
+        // Check if other branch has this arch
+        if (!other.data.contains(it.key()))
+            continue;
+
+        result.push_back(it.key());
+        // Cycle over packets in one arch
+        for (const auto &pkg : it.value().items())
+            if (other.data[it.key()].contains(pkg.key())) {
+                // Copy package and store it with name
+                json package{pkg.value()};
+                package["name"] = pkg.key();
+                result[it.key()].push_back(std::move(package));
+            }
+    }
+
+    return result;
+}
--- core/lib.hpp
+++ core/lib.hpp
@@ -1,18 +1,33 @@
 #pragma once
 
+#include "nlohmann/json_fwd.hpp"
 #include <nlohmann/json.hpp>
 
 #include <string>
 #include <optional>
 
 using json = nlohmann::json;
+using ojson = nlohmann::ordered_json;
 
 /// Class for getting json from branch name
 class ALTAPI {
 public:
-    ALTAPI(std::string url);
+    explicit ALTAPI(std::string url);
 
-    std::optional<json> get(const std::string &api) const;
+    std::optional<json> get(const std::string &) const;
 private:
     std::string base_url;
+};
+
+
+/// Class for parsing branch info
+/// Utilizes ordered structure of request
+class BranchData {
+public:
+    explicit BranchData(json);
+
+    json substract(const BranchData &) const;
+
+private:
+    ojson data;
 };
